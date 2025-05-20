48df5626caef36dc487e5a275586e38ed704dace
jdriscoll98 <41447166+jdriscoll98@users.noreply.github.com>
2025-05-19 22:56:29 -0400

    fix countdown when end time updates

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
 
 export function useCountdown(endTime: number) {
-  const [timeLeft, setTimeLeft] = useState(
-    endTime ? Math.floor((endTime - Date.now()) / 1000) : 0
-  );
+  const calculateSeconds = () =>
+    endTime ? Math.max(0, Math.floor((endTime - Date.now()) / 1000)) : 0;
+
+  const [timeLeft, setTimeLeft] = useState(calculateSeconds());
+
   useEffect(() => {
     const interval = setInterval(() => {
       if (!endTime) return;
-      const seconds = Math.floor((endTime - Date.now()) / 1000);
+      const seconds = calculateSeconds();
       setTimeLeft(seconds);
+      if (seconds <= 0) {
+        clearInterval(interval);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [endTime]);
 
   const daysLeft = Math.floor(timeLeft / 86400);
   const hoursLeft = Math.floor((timeLeft % 86400) / 3600);
   const minutesLeft = Math.floor((timeLeft % 3600) / 60);
   const secondsLeft = timeLeft % 60;
 
-  return `
-    ${daysLeft.toString().padStart(2, "0")}:${hoursLeft
+  return `${daysLeft.toString().padStart(2, "0")}:${hoursLeft
     .toString()
     .padStart(2, "0")}:${minutesLeft.toString().padStart(2, "0")}:${secondsLeft
     .toString()
